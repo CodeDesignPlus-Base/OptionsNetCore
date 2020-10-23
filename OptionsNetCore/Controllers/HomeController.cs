5d269db6a9e6eb39b0d1e4c676ab7bd1209e9474
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-10-22 22:22:00 -0500

    Agregar archivos de proyecto.

---
+++ OptionsNetCore/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using OptionsNetCore.Core.Options.Redis;
+using OptionsNetCore.Services;
+using System;
+
+namespace OptionsNetCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IOptions<RedisOptions> _options;
+        private readonly IOptionsSnapshot<RedisOptions> _snapshot;
+        private readonly IOptionsMonitor<RedisOptions> _monitor;
+
+        public HomeController(
+            IOptions<RedisOptions> options,
+            IOptionsSnapshot<RedisOptions> snapshot,
+            IOptionsMonitor<RedisOptions> monitor)
+        {
+            this._options = options;
+            this._snapshot = snapshot;
+            this._monitor = monitor;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = new
+            {
+                options = this._options.Value,
+                snapshot = this._snapshot.Value,
+                monitor = this._monitor.CurrentValue
+            };
+
+            return Ok(data);
+        }
+    }
+}
---
+++ OptionsNetCore/Core/Options/Redis/RedisOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace OptionsNetCore.Core.Options.Redis
+{
+    /// <summary>
+    /// Class that contains the options to configure Redis
+    /// </summary>
+    public class RedisOptions
+    {
+        /// <summary>
+        /// Identificador de la sección en el archivo de configuración
+        /// </summary>
+        public const string Redis = "RedisSettings";
+
+        public bool ResolveDns { get; set; }
+        public string EndPoints { get; set; }
+        public int ConnectRetry { get; set; }
+        public int ConnectTimeout { get; set; }
+        public int SyncTimeout { get; set; }
+    }
+}
